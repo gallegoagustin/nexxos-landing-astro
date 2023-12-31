4d1b8b47a6a681448622a3fb821b71e5a212ad43
gallegoagustin <gallegoagustin7@gmail.com>
2023-12-31 11:11:00 +0000

    added prettier & eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+};
