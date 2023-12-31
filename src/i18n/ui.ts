02ff83f74b35816eb637051d52d4dec9b0b661ce
gallegoagustin <gallegoagustin7@gmail.com>
2023-12-31 12:55:52 +0000

    added i18n config

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'nav.home': 'Home',
+        'nav.about': 'Sobre nosotros',
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+    },
+} as const;
